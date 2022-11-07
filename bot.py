67e723df384d9dc4c9591f25adae2e3c657a55be
Leo <dev.rignaneseleo@gmail.com>
2022-11-07 21:07:41 +0100

    added inline_query

--- bot.py
+++ bot.py
@@ -1,20 +1,56 @@
 import logging
 import os
+import script
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler, InlineQueryResultArticle, InputTextMessageContent
 
 
 def start(update, context):
-    update.effective_message.reply_text("Hi!")
+    update.effective_message.reply_text(
+        "Hi there, this bot helps you get a link of your flight, so that your mom doesn't get worried!")
+
+# Handle all other messages
+def reply(update, context):
+    update.effective_message.reply_text("""This bot can be used only in the chat.
+Use @FlightLinkBot in the chat to get a link of your flight.
+    
+Examples:
+@FlightLinkBot Milan Madrid
+@FlightLinkBot BGY - Madrid
+@FlightLinkBot BGY MDR""")
 
 
-def echo(update, context):
-    update.effective_message.reply_text(update.effective_message.text)
+# Handle inline texts
+def inline_query(update, context) -> None:
+    message = "✈️ Follow my {} flight from {} to {}:\r\n\r\n{}"
+    #if query.from_user.language_code == 'it':
+    #    message = "✈️ Segui il mio volo {} da {} a {}:\r\n\r\n{}"
+    text = update.inline_query.query
+    routes = script.find_route(text)
+    results = []
+
+    for route in routes:
+        r = InlineQueryResultArticle(
+            # The id of our inline result
+            thumb_url=route.getIconUrl(),
+            id=routes.index(route),
+            title="[%s] %s (%s) - %s (%s)" % (route.code_IATA,
+                                              route.departure_airport.city, route.departure_airport.code_iata,
+                                              route.arrival_airport.city, route.arrival_airport.code_iata),
+            input_message_content=InputTextMessageContent(message
+                                                                .format(route.airline.name, route.departure_airport.city, route.arrival_airport.city, route.getFlightAwareLink())
+                                                                )
+        )
+        results.append(r)
+    print("Sending results to query: " + text)
+    # show the choices
+    update.inline_query.answer(results)
 
 
 if __name__ == "__main__":
     # heroku app name
     NAME = "flight-link-bot"
+    # get TOKEN from Heroku Config Vars
     TOKEN = os.environ['TOKEN']
 
     # Port is given by Heroku
@@ -30,7 +66,10 @@ if __name__ == "__main__":
     dp = updater.dispatcher
     # Add handlers
     dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, reply))
+
+     # on non command i.e message - echo the message on Telegram
+    dp.add_handler(InlineQueryHandler(inline_query))
 
     # Start the webhook
     updater.start_webhook(listen="0.0.0.0",
